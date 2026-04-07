6a8d042d60e159ff0c8a18a80f1eca5d51630a2a
hailegithubaccount <michaelhaile1222@gmail.com>
2026-04-07 14:31:11 +0300

    adding the filte

--- api/filters.py
+++ api/filters.py
@@ -2,9 +2,9 @@ import django_filters
 from api.models import Product
 from rest_framework import filters
 
-# class InStockFilterBackend(filters.BaseFilterBackend):
-#     def filter_queryset(self, request, queryset, view):
-#         return queryset.filter(stock__gt=0)
+class InStockFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(stock__gt=0)
 
         # if it's filter = it displayed all products but not the stock is zero
         #if it's exculded= it displayed the stock is zero
